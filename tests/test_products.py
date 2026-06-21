c3496feb2afb38058f6836834b6759de7b270e92
Irene <irenearribasmendez@gmail.com>
2026-06-21 13:02:02 +0200

    Update test_products.py

--- tests/test_products.py
+++ tests/test_products.py
@@ -10,14 +10,17 @@ def test_products_catalog(page: Page):
     expect(page).to_have_title("Nuestros Productos | Vida Verde")
     
     print ("Then the user sees the title “Products catalog”")
-    locator = page.get_by_role("heading", name="Catálogo de Productos")
+    locator = page.get_by_role("heading", name = "Catálogo de Productos")
     expect(locator).to_have_text("Catálogo de Productos")
 
     print ("Then the user sees the category “plants” on the products within that category")
-    page.get_by_text("Plantas")
+    locator = page.get_by_text("Plantas").nth(2)
+    expect(locator).to_have_text("Plantas")
     
     print ("And the user sees a product with the name “Ficus Lyrata”")
-    page.get_by_role("heading", name="Ficus Lyrata")
+    locator = page.get_by_role("heading", name = "Ficus Lyrata")
+    expect(locator).to_contain_text("Ficus Lyrata")
 
     print ("And the user sees that the product's price is “35.00€”")
-    page.get_by_text("35.00 €")
+    locator = page.get_by_text("35.00 €")
+    expect(locator).to_contain_text("35.00 €")
