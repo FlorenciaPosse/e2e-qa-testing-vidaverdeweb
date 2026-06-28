ecd52b4bd4dba9262f06283267823cb70ab9288e
Sara Posada <saraposadadominguez@gmail.com>
2026-06-28 12:05:03 +0100

    Update test purchase with valid data

--- tests/test_purchase_valid_data.py
+++ tests/test_purchase_valid_data.py
@@ -5,35 +5,35 @@ def test_checkout_purchase_with_valid_data(page: Page):
     
     print('And filters products by the name "palas"')
     page.get_by_placeholder("Buscar productos...").fill("palas")
+    expect(page.get_by_text("Juego de Palas")).to_be_visible()
 
     print("And adds the product to the cart")
-    page.get_by_placeholder("Buscar productos...").fill("palas")
-    page.get_by_role("button", name="Añadir al carrito").first.click()
+    page.get_by_text("Añadir al carrito").first.click()
     
     print('And clicks on "Finalizar compra"')
-    page.get_by_role("button", name="Finalizar compra").click()
+    page.get_by_text("Finalizar compra").first.click()
     expect(page.get_by_text("Resumen del pedido")).to_be_visible()
 
     print('And clicks on "Proceder al pago"')
-    page.get_by_role("button", name="Proceder al pago").click()
+    page.get_by_text("Proceder al pago").first.click()
 
     print('Then the order summary should display the product "Juego de Palas"')
     expect(page.get_by_text("Juego de Palas")).to_be_visible()
    
     print('And the product price should be "15.99"')
-    expect(page.get_by_text("15.99")).to_be_visible()
+    expect(page.get_by_text("15.99").first).to_be_visible()
 
     print('And the subtotal should be "15.99"')
-    expect(page.get_by_text("15.99")).to_be_visible()
+    expect(page.get_by_text("15.99").last).to_be_visible()
 
     print('And the VAT should be "3.36"')
     expect(page.get_by_text("3.36")).to_be_visible()
 
-    print('And the shipping cost should be "5"')
+    print('And the shipping cost should be "5.00"')
     expect(page.get_by_text("5.00")).to_be_visible()
 
     print('And the total amount should be "24.35"')
-    expect(page.get_by_text("24.35")).to_be_visible()
+    expect(page.get_by_text("24.35").first).to_be_visible()
 
     print('When the user enters the name "Reyes Maria"')
     page.get_by_label("Nombre Completo").fill("Reyes Maria")
@@ -57,7 +57,7 @@ def test_checkout_purchase_with_valid_data(page: Page):
     expect(page.get_by_text("24.35")).to_be_visible()
 
     print('When the user clicks on "Ir al inicio"')
-    page.get_by_role("button", name="Ir al inicio").click()
+    page.get_by_text("Ir al inicio").first.click()
 
     print("Then the user should be redirected to the home page")
     expect(page).to_have_url("https://web-qa.dev.adalab.es/")
